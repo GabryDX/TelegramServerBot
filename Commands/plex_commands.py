7f1b8be3be65367846069f24a5448ba5c4cad941
GabryDX <gabrydx@hotmail.com>
2023-06-13 23:16:10 +0200

    Update for better Plex Library

--- Commands/plex_commands.py
+++ Commands/plex_commands.py
@@ -4,9 +4,16 @@ from Utils.server_info import run_command
 
 def get_plex_library():
     command = 'find "' + constants.CUSTOM.PLEX_MEDIA_FOLDER \
-              + '" | sed -e "s/[^-][^\\/]*\\//  |/g" -e "s/|\\([^ ]\\)/|-\1/"'
+              + '" | sed -e "s/[^-][^\\/]*\\// |/g" -e "s/|\\([^ ]\\)/|-\1/"'
     response = run_command(command)
-    return response
+    splitted = response.split("\n")
+    init = splitted[0]
+    len_init = len(init)
+    new_response = ""
+    for s in splitted[1:]:
+        if init in s:
+            new_response += s[len_init:].strip() + "\n"
+    return new_response
 
 
 def update_plex_library():
